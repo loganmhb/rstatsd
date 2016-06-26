72a84acdfa2a7e6f42ad5c8abad5f7b027f81cd9
Logan Buckley <lbuckley@opinionlab.com>
2016-06-25 20:28:25 -0400

    Collect UDP messages in a flushable buffer

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,25 @@
 extern crate rstatsd;
 
+use std::thread;
+use std::time::Duration;
+
 use rstatsd::servers::udp;
+use rstatsd::queue::StatsQueue;
+
 
 fn main() {
     println!("totally running statsd! trust me!");
-    udp::listen_on_socket(("localhost", 34254));
+    let queue = StatsQueue::new();
+    let queue_handle = queue.clone();
+    let server_handle = thread::spawn(move || {
+        udp::collect_udp_messages(("localhost", 34254), queue_handle);
+    });
+
+    loop {
+        thread::sleep(Duration::from_millis(10000));
+        let buf = queue.flush();
+        for msg in buf {
+            println!("{}", msg);
+        }
+    }
 }
--- src/servers/udp.rs
+++ src/servers/udp.rs
@@ -1,16 +1,23 @@
 use std::net::{UdpSocket, ToSocketAddrs};
 use std::io;
+use std::vec::Vec;
 use std::io::Write;
-use std::str;
+use std::string::String;
+
+use queue::StatsQueue;
 
 
-pub fn listen_on_socket<A: ToSocketAddrs>(addr: A) -> {
+pub fn collect_udp_messages<A: ToSocketAddrs>(addr: A, stats_buffer: StatsQueue) {
     let socket = UdpSocket::bind(addr).unwrap();
     loop {
         let mut buf = [0; 512];
         let res = socket.recv_from(&mut buf);
         match res {
-            Ok((amt, _)) => println!("{}", str::from_utf8(&buf[0..amt]).unwrap()),
+            Ok((amt, _)) => {
+                let mut v: Vec<u8> = Vec::new();
+                v.extend_from_slice(&buf[0..amt]);
+                stats_buffer.push(String::from_utf8(v).unwrap())
+            },
             Err(e) => write!(io::stderr(), "{}", e).unwrap()
         }
     }
